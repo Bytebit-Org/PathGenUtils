0549147ed6fd9f36be5f8e7a6bdef7eef2c0d9b1
Noah Crowley <noahwillcrow@gmail.com>
2019-08-03 19:47:24 -0700

    Add derivative-based smoothing

--- out/Interfaces/IPathData.lua
+++ out/Interfaces/IPathData.lua
@@ -1,13 +1,14 @@
 -- Compiled with https://roblox-ts.github.io v0.2.14
--- August 1, 2019, 6:31 PM GMT-08:00
+-- August 3, 2019, 7:47 PM Pacific Daylight Time
 
 local TS = _G[script];
 local exports;
 local t = TS.import(TS.getModule("t").lib.ts);
-local tWaypoint = TS.import(script.Parent, "tWaypoint");
-exports = t.interface({
+local tWaypoint = TS.import(script.Parent, "IWaypoint");
+local IPathData = t.interface({
 	Name = t.string;
 	TotalDistance = t.numberMinExclusive(0);
 	Waypoints = t.array(tWaypoint);
 });
+exports = IPathData;
 return exports;
