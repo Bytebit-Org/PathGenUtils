a1f80bfecb354dfe90032f6ba18344e670c9e6d2
Noah Crowley <noahwillcrow@gmail.com>
2019-10-15 20:49:42 -0700

    add out to node_modules again

---
+++ out/Interfaces/IWaypoint.lua
@@ -0,0 +1,14 @@
+-- Compiled with https://roblox-ts.github.io v0.2.14
+-- October 15, 2019, 8:49 PM Pacific Daylight Time
+
+local TS = _G[script];
+local exports;
+local t = TS.import(TS.getModule("t").lib.ts);
+local IWaypoint = t.interface({
+	DistanceProgress = t.numberConstrained(0, 1);
+	RelativePosition = t.Vector3;
+	ExitingHandleRelativePosition = t.optional(t.Vector3);
+	EnteringHandleRelativePosition = t.optional(t.Vector3);
+});
+exports = IWaypoint;
+return exports;
